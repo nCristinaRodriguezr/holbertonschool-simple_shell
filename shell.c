89e6d78ac901568bce0110a2c60135457622431e
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-21 15:35:01 -0700

    testing space

--- shell.c
+++ shell.c
@@ -24,7 +24,7 @@ void exec_token(char *input)
 		i++;
 	}
 	args[i] = NULL;
-	if (args[0] != NULL && args[0] != " ")
+	if (args[0] != NULL && strcmp(args[0], " ") != 0)
 	{
 		execve(args[0], args, NULL);
 	}
