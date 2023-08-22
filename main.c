1eabd884e164324a68021651837504683a016b8c
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-21 18:24:32 -0700

    testing final string

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#define MAX_INPUT_SIZE 1024
 #include "main.h"
 /**
  * main- esta función interpreta el comando el usuario
@@ -14,6 +15,8 @@ int main(void)
 	char *input = NULL;
 	size_t len = 0;
 	ssize_t read;
+	char *comands[MAX_INPUT_SIZE];
+	char *miniInput = NULL;
 	pid_t pid;
 
 	while (1)
@@ -22,25 +25,23 @@ int main(void)
 			write(1, "($)", 3);
 		read = getline(&input, &len, stdin);
 		if (read == -1)
-		{
 			break;
-		}
-		strtrim(input);
-		input[read - 1] = '\0';
-		pid = fork();
-		if (pid < 0)
+		tokenizeInput(input, comands, "\n");
+		while (comands[i] != NULL)
 		{
-			perror("Error al crear el proceso hijo");
+			miniInput = *comands[i];
+			strtrim(miniInput);
+			miniInput[read - 1] = '\0';
+			pid = fork();
+			if (pid < 0)
+				perror("Error al crear el proceso hijo");
+			else if (pid == 0)
+				exec_token(miniInput);
+			else
+				waitpid(pid, NULL, 0);
+			i++;
 		}
-		else if (pid == 0)
-		{
-			exec_token(input);
-		}
-		else
-		{
-			waitpid(pid, NULL, 0);
-		}
-	}
+	}	
 	free(input);
 	return (0);
 }
