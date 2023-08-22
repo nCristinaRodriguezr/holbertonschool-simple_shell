24bd7949502ca332a6e93015bc18efa2e58da12d
nataly rodriguez <ncristina.rodriguezr@gmail.com>
2023-08-22 10:32:51 -0700

    new testing

--- shell.c
+++ shell.c
@@ -32,13 +32,13 @@ void exec_token(char *input)
 void strtrim(char *input)
 {
 	int i = strlen(input) - 1;
-	while (i >= 0 && (input[i] == ' ' || input[i] == '\n'))
+	while (i >= 0 && (input[i] == ' ' || input[i] == '\n' || input[i] == '\0'))
 	{
 		input[i] = '\0';
 		i--;
 	}
 	i = 0;
-	while (input[i] && (input[i] == ' ' || input[i] == '\n'))
+	while (input[i] && (input[i] == ' ' || input[i] == '\n' || input[i] == '\0'))
 	{
 		i++;
 	}
