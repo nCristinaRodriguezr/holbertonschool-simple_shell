19fc1cb14f346290f67a28edd0f080eb41d197db
JBMjese <6935@holbertonstudents.com>
2023-08-23 16:50:08 -0700

    testing env

---
+++ environ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "main.h"
+/**
+  * builtin_env - prints the environment
+  */
+void builtin_env(void)
+{
+	extern char **environ;
+
+	for (int i = 0; environ[i] != NULL; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
+}	
